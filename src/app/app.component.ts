57b2bc8e71b45e87e3c1c14b5e14917f6c839760
Juan David Calderón Jiménez <juandacalji@gmail.com>
2021-11-25 11:06:59 -0500

    Galeria Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'galeria';
+  fotos: string[] = ["../assets/galeria/01.jpg", "../assets/galeria/02.jpg", "../assets/galeria/03.jpg", "../assets/galeria/04.jpg", "../assets/galeria/05.jpg", "../assets/galeria/06.jpg", "../assets/galeria/07.jpg", "../assets/galeria/08.jpg", "../assets/galeria/09.jpg", "../assets/galeria/10.jpg", "../assets/galeria/11.jpg", "../assets/galeria/12.jpg", "../assets/galeria/13.jpg", "../assets/galeria/14.jpg", "../assets/galeria/15.jpg", "../assets/galeria/16.jpg", "../assets/galeria/17.jpg", "../assets/galeria/18.jpg", "../assets/galeria/19.jpg", "../assets/galeria/20.jpg", "../assets/galeria/21.jpg"];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  @Input() image: string;
+
+  constructor() {
+    this.image = ""
+  }
 
   ngOnInit(): void {
   }
